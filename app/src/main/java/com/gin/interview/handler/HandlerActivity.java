bbe6bb33b5349c8fdbbdd7d0b4a264b5f54c7230
DB_BOY <ginngi_wang@163.com>
2017-07-19 15:10:19 +0800

    子线程间通信

--- app/src/main/java/com/gin/interview/handler/HandlerActivity.java
+++ app/src/main/java/com/gin/interview/handler/HandlerActivity.java
@@ -2,7 +2,11 @@ package com.gin.interview.handler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.gin.interview.R;
 
@@ -11,18 +15,41 @@ import com.gin.interview.R;
  */
 
 public class HandlerActivity extends Activity {
-    
-    
+    Handler childHandler;
     
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler);
-        
+
+        new Thread1().start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Message msg = childHandler.obtainMessage();
+                msg.obj = " eeeee";
+                childHandler.sendMessage(msg);
+            }
+        }).start();
         
     }
-    
-    
+
+    class Thread1 extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            Looper.prepare();
+
+            childHandler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    Log.i("---tag", "msg: " + msg.obj);
+                }
+            };
+            Looper.loop();
+        }
+    }
     
     
 }
